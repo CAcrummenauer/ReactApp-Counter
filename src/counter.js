d82f294089692bd58856f72113d42af3f397b95a
Cezar Augusto Crummenauer <cezar.augusto.web@gmail.com>
2019-01-25 20:38:36 -0200

    Contador versão 3...

--- src/counter.js
+++ src/counter.js
@@ -2,27 +2,27 @@ import React from 'react';
 
 class Counter extends React.Component {
     state = {
-        counter: 0,
+        counter: 0
     };
 
+    handlerIncrement() {
+        this.setState({
+            counter: this.state.counter + 1
+        });
+    }
+
+    handlerDecrement() {
+        this.setState({
+            counter: this.state.counter - 1
+        });
+    }
+
     render() {
-        const handlerIncrement = () => {
-            this.setState({
-                counter: this.state.counter + 1
-            });
-        }
-    
-        const handlerDecrement = () => {
-            this.setState({
-                counter: this.state.counter - 1
-            })
-        }
-        
         return (
             <div>
-                <button type="button" onClick = {handlerIncrement}>+</button>
+                <button type="button" onClick = {() => this.handlerIncrement()}>+</button>
                 <span>{this.state.counter}</span>
-                <button type="button" onClick = {handlerDecrement}>-</button>
+                <button type="button" onClick = {() => this.handlerDecrement()}>-</button>
             </div>
         );
     }
